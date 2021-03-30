7b3f8fcf20a5ac1ec1d3eddd7756eb8bc2caa876
Vladimir <karlov.v01@gmail.com>
2021-03-30 23:40:51 +0300

    push

--- Push/Code/calculations_push.py
+++ Push/Code/calculations_push.py
@@ -8,28 +8,15 @@ import time
 
 
 class Point:
-    def __init__(self, real_value):
+    def __init__(self, real_value, predictions_set, predicted_value, is_virgin, is_completed):
         self.real_value = real_value
-
-
-class VirginPoint(Point):
-    def __init__(self, real_value):
-        super().__init__(real_value)
-
-
-class MiddlePoint(Point):
-    def __init__(self, real_value, predictions_set, predicted_value):
-        super().__init__(real_value)
         self.predictions_set = predictions_set
         self.predicted_value = predicted_value
+        self.is_virgin = is_virgin
+        self.is_completed = is_completed
 
-    def add_prediction(self, prediction):
-        self.predictions_set = np.append(self.predictions_set, prediction)
-
-
-class CompletedPoint(Point):
-    def __init__(self, real_value):
-        super().__init__(real_value)
+    def info(self):
+        print('{ ', self.predictions_set.size, self.real_value, self.predicted_value, '}', end=' ')
 
 
 def normalize(arr):
@@ -47,29 +34,26 @@ NUMBER_OF_CLAWS = 4
 TRAIN_GAP = 1000
 TEST_GAP = 1
 
-MAX_NORM_DELTA = 0.02  # было 0.015
-MAX_ABS_ERROR = 0.15  # изначально было 0.05
+MAX_NORM_DELTA = 0.015  # было 0.015
+MAX_ABS_ERROR = 0.05  # изначально было 0.05
 
 S = 34  # количество предшедствующих точек ряда, необходимое для прогнозирования точки
 
 K_MAX = 4
 
-
-# @jit
-def reforecast(points, predictions_sets, first_not_absolute, i, k):
+@jit
+def reforecast(points, first_not_completed):
     print("\nreforcasting started:")
     for template_number in range(len(templates_by_distances)):
         x, y, z = templates_by_distances[template_number]
-        for middle_point in range(first_not_absolute, len(points)):  # middle_point - это индекс в points
-            pred_sets_index = middle_point - first_not_absolute
-
-            if z >= pred_sets_index:  # +-1
+        for middle_point in range(first_not_completed, len(points)):  # middle_point - это индекс в points
+            if middle_point + z + 1 >= len(points) or points[middle_point].is_virgin or points[middle_point + z + 1].is_completed:
                 continue
 
             left_part = np.array(
-                [points[middle_point - z - y - x - 3],
-                 points[middle_point - z - y - 2],
-                 points[middle_point - z - 1]]
+                [points[middle_point - y - x - 2].predicted_value,
+                 points[middle_point - y - 1].predicted_value,
+                 points[middle_point].predicted_value]
             )
 
             if np.isnan(np.sum(left_part)):
@@ -78,27 +62,30 @@ def reforecast(points, predictions_sets, first_not_absolute, i, k):
 
             for shifted_template in shifts_for_each_template[template_number]:
                 if np.linalg.norm(left_part - shifted_template[:3]) <= MAX_NORM_DELTA:
-                    predictions_sets[pred_sets_index].append(shifted_template[3])
+                    points[middle_point + z + 1].predictions_set = np.append(points[middle_point + z + 1].predictions_set, shifted_template[3])
+                    points[middle_point + z + 1].is_virgin = False
 
-    print("sets are changed, repredicting values:")
-    for middle_point in range(first_not_absolute, len(points)):
-        print("  recalculating point", middle_point, pred_sets_index)
-        pred_sets_index = middle_point - first_not_absolute
+    for middle_point in range(first_not_completed, len(points)):
+        print("  recalculating point", middle_point, )
+        point_obj = points[middle_point]
 
-        if predictions_sets[pred_sets_index]:
-            predicted_value = sum(predictions_sets[pred_sets_index]) / len(predictions_sets[pred_sets_index])
+        if point_obj.predictions_set.size:
+            point_obj.predicted_value = sum(point_obj.predictions_set) / len(point_obj.predictions_set)
         else:
-            predicted_value = np.nan
+            continue
 
-        cur_error = abs(LORENZ[i - k + middle_point - S + 1] - predicted_value)
-        if np.isnan(predicted_value) or (cur_error > MAX_ABS_ERROR and middle_point != len(points) - 1):
-            points[middle_point] = np.nan
-            print("%d-th point is unpredictable, error = %f" % (middle_point, cur_error))
-        else:
-            points[middle_point] = predicted_value
-            print("%d-th point is predictable, predicted_value: %f, error = %f" % (middle_point, predicted_value, cur_error))
-        print([len(cur_set) for cur_set in predictions_sets])
+        cur_error = abs(point_obj.real_value - point_obj.predicted_value)
 
+        if np.isnan(point_obj.predicted_value) or (cur_error > MAX_ABS_ERROR and middle_point != len(points) - 1):
+            point_obj.predicted_value = np.nan
+            # print("%d-th point is unpredictable, error = %f" % (middle_point, cur_error))
+
+        # print("%d-th point is predictable, predicted_value: %f, error = %f" % (middle_point, predicted_value, cur_error))
+    for printed_point_index in range(S, len(points)):
+        points[printed_point_index].info()
+    print('\n')
+
+    points[first_not_completed].is_completed = True
     return points
 
 
@@ -106,19 +93,23 @@ def reforecast(points, predictions_sets, first_not_absolute, i, k):
 def predict(i, k):
     print("cur_point = 0:".upper())
     # last_predicted_index = S  # индекс в points последней точки, в который был получен абсолютный прогноз +-1
-    complete_points = [CompletedPoint(i, ) for i in range(LORENZ[i - k - 33: i - k + 1])]  # правая граница не включена => это список из 34 + k точек
-    new_points = [VirginPoint(i) for i in range(LORENZ[i - k + 1: i + k + 1])]
+    complete_points = [Point(_, np.array([]), _, 0, 1) for _ in LORENZ[i - k - 33: i - k + 1]]  # правая граница не включена => это список из 34 + k точек
+    new_points = [Point(_, np.array([]), np.nan, 1, 0) for _ in LORENZ[i - k + 1: i + 1]]
     points = complete_points + new_points
     # нулевая итерация
-    points = fill(points, predictions_sets, i, k)
+
+
+    reforecast(points, S - 10)
+    # points = reforecast(points, S, i, k)
+
+
+
     # тут необходимо также добавить одно абсолютное значение
 
     for cur_point in range(1, k):
         print("\n\ncur_point = ".upper(), cur_point, ":", sep='')
         # print("cur_point: ", cur_point)
-        points = reforecast(points, predictions_sets, S + cur_point, i, k)
-        points = fill(points, predictions_sets, i, k)
-        # print("predictions_sets len:", [len(_) for _ in predictions_sets])
+        points = reforecast(points, S + cur_point)
 
     # import matplotlib.pyplot as plt
     #
@@ -126,28 +117,28 @@ def predict(i, k):
     # plt.plot(np.linspace(0, k, k), LORENZ[i - k:i], color='red')
     # plt.show()
 
-    return abs(LORENZ[i] - points[-1]), not np.isnan(points[-1])
+    return abs(LORENZ[i] - points[-1].predicted_value), not np.isnan(points[-1].predicted_value)
 
 
 def process_for_each_k(k):
     sum_of_abs_errors = 0
-    nubmer_of_unpredictable = 0
+    number_of_unpredictable = 0
 
     for i in range(TEST_BEGIN, TEST_BEGIN + TEST_GAP):  # till TEST_END + 1
         (error, is_predictable) = predict(i, k)
-        # print("(error, is_predictable):", (error, is_predictable), '\n')
+        print("(error, is_predictable):", (error, is_predictable), '\n')
         if (is_predictable):
             sum_of_abs_errors += error
         else:
-            nubmer_of_unpredictable += 1
+            number_of_unpredictable += 1
 
-    if (nubmer_of_unpredictable == TEST_GAP):
+    if number_of_unpredictable == TEST_GAP:
         k_RMSE = np.nan
     else:
-        k_RMSE = sum_of_abs_errors / (TEST_GAP - nubmer_of_unpredictable)
+        k_RMSE = sum_of_abs_errors / (TEST_GAP - number_of_unpredictable)
 
-    print("k =", k, k_RMSE, nubmer_of_unpredictable / TEST_GAP)
-    return (k_RMSE, nubmer_of_unpredictable / TEST_GAP)
+    print("k =", k, k_RMSE, number_of_unpredictable / TEST_GAP)
+    return (k_RMSE, number_of_unpredictable / TEST_GAP)
 
 
 # Generating templates
@@ -166,17 +157,15 @@ for template_number in range(len(templates_by_distances)):
 # file_rmse = open("RMSE.txt", 'w')
 # file_percent_of_unpredictable = open("percent_of_unpredictable.txt", 'w')
 
-# works = range(1, K_MAX + 1)
+works = range(1, K_MAX + 1)
 
 # if __name__ == '__main__':
 #     with Pool(processes=4) as pool:
 #         res = pool.map(process_for_each_k, works)
 #         print(res)
 
-# for work in works:
-#     process_for_each_k(work)
-
-predict(99950, 15)
+for work in works:
+    process_for_each_k(work)
 
 # file_rmse.close()
 # file_percent_of_unpredictable.close()
