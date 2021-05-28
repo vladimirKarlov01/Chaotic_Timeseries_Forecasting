5620fd46352acb45ded8cd6bc62aeef6bb29bd01
Vladimir <karlov.v01@gmail.com>
2021-05-28 11:48:04 +0300

    push graphics

--- Push/Code/parse.py
+++ Push/Code/parse.py
@@ -1,8 +1,9 @@
 def is_normal_line(line):
     return not line == "\n"
 
+
 def parse():
-    file = open("Testing_results/28-04-2021-push-id-delta-0.007-err-0.05-mean/push-id-delta-0.007-err-0.05/push-id-delta-0.007-err-0.05.txt")
+    file = open("Testing_results/push-mean-10000-100/push-id-delta-0.002-err-0.005/push.txt", "r")
     lines = file.readlines()
     lines = list(filter(is_normal_line, lines))
     for i in range(len(lines)):
@@ -11,9 +12,9 @@ def parse():
     lines.sort()
     # print(lines)
 
-    points = open("Testing_results/28-04-2021-push-id-delta-0.007-err-0.05-mean/push-id-delta-0.007-err-0.05/points.txt", "w")
-    rmse = open("Testing_results/28-04-2021-push-id-delta-0.007-err-0.05-mean/push-id-delta-0.007-err-0.05/RMSE.txt", "w")
-    unpred_points_percents = open("Testing_results/28-04-2021-push-id-delta-0.007-err-0.05-mean/push-id-delta-0.007-err-0.05/percent_of_unpredictable.txt", "w")
+    points = open("Testing_results/push-mean-10000-100/push-id-delta-0.002-err-0.005/points.txt", "w")
+    rmse = open("Testing_results/push-mean-10000-100/push-id-delta-0.002-err-0.005/RMSE.txt", "w")
+    unpred_points_percents = open("Testing_results/push-mean-10000-100/push-id-delta-0.002-err-0.005/percent_of_unpredictable.txt", "w")
     for line in lines:
         print(line[0], file=points)
         print(line[1], file=rmse)
--- Push/Code/plotting.py
+++ Push/Code/plotting.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# no unpredictable points
-points1 = np.genfromtxt("Testing_results/28-04-2021-push-id-delta-0.007-err-0.05-mean/push-id-delta-0.007-err-0.05-no-unpred/points.txt")
-rmse1 = np.genfromtxt("Testing_results/28-04-2021-push-id-delta-0.007-err-0.05-mean/push-id-delta-0.007-err-0.05-no-unpred/RMSE.txt")
-unpred_points_percent1 = np.genfromtxt("Testing_results/28-04-2021-push-id-delta-0.007-err-0.05-mean/push-id-delta-0.007-err-0.05-no-unpred/percent_of_unpredictable.txt")
+# ab
+points1 = np.genfromtxt("Testing_results/push-mean-10000-100/push-ab/points.txt")
+rmse1 = np.genfromtxt("Testing_results/push-mean-10000-100/push-ab/RMSE.txt")
+unpred_points_percent1 = np.genfromtxt("Testing_results/push-mean-10000-100/push-ab/percent_of_unpredictable.txt")
 
-# with unpredictable points
-points2 = np.genfromtxt("Testing_results/28-04-2021-push-id-delta-0.007-err-0.05-mean/push-id-delta-0.007-err-0.05/points.txt")
-rmse2 = np.genfromtxt("Testing_results/28-04-2021-push-id-delta-0.007-err-0.05-mean/push-id-delta-0.007-err-0.05/RMSE.txt")
-unpred_points_percent2 = np.genfromtxt("Testing_results/28-04-2021-push-id-delta-0.007-err-0.05-mean/push-id-delta-0.007-err-0.05/percent_of_unpredictable.txt")
+# id
+points2 = np.genfromtxt("Testing_results/push-mean-10000-100/push-id-delta-0.002-err-0.005/points.txt")
+rmse2 = np.genfromtxt("Testing_results/push-mean-10000-100/push-id-delta-0.002-err-0.005/RMSE.txt")
+unpred_points_percent2 = np.genfromtxt("Testing_results/push-mean-10000-100/push-id-delta-0.002-err-0.005/percent_of_unpredictable.txt")
 
 
 fig, (ax1, ax2) = plt.subplots(
